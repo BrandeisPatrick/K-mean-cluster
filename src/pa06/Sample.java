66ef0975710e98e36db1949673a8af6cf313252c
denisezhong <62267168+denisezhong@users.noreply.github.com>
2020-04-13 19:39:54 -0400

    Attempted toString method

--- src/pa06/Sample.java
+++ src/pa06/Sample.java
@@ -19,15 +19,26 @@ public class Sample {
 		
 	}
 	
+	public String toString() {
+		String string = "{";
+		for(int i=0; i<this.size(); i++) {
+			string += this.get(i)+",";
+		}
+		string += "}";
+		return string;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("testing for the Sample class.");
 		double[] p1 = {1d, 2d, 3.14, 2.71};
 		double[] p2 = {0.1, 1.1, 2.1, 3.1};
 		Sample s1 = new Sample(p1);
 		Sample s2 = new Sample(p2);
-		System.out.println("s1="+s1);
-		System.out.println("s2="+s2);
+		System.out.println("s1="+s1.toString());
+		System.out.println("s2="+s2.toString());
 		
 	}
+	
+
 
 }

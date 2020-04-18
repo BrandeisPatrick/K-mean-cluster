cdde2e9556c8f3cb04527e2ad021aa56ed6e631b
kyrarivest <57569515+kyrarivest@users.noreply.github.com>
2020-04-18 19:40:18 -0400

    Fixed the distance method

--- src/pa06/Sample.java
+++ src/pa06/Sample.java
@@ -43,22 +43,23 @@ public class Sample implements Comparable<Sample> {
 	}
 	
 	public double distanceTo(Sample s2) { //needs interface if your parameter is assumed to be Sample type
-		double total = 0;
+		double sum = 0;
 		for(int i = 0; i<s2.sample.size(); i++) {
-			total += Math.sqrt(Math.abs(this.sample.get(i)-s2.sample.get(i)));
+			sum += Math.pow(this.sample.get(i)-s2.sample.get(i),2);
 		}
-		return Math.sqrt(total);
+		return Math.sqrt(sum);
 	}
 
 	//test (static method)
 	public static void main(String[] args) {
 		System.out.println("testing for the Sample class.");
-		double[] p1 = {1d, 2d, 3.14, 2.71};
-		double[] p2 = {0.1, 1.1, 2.1, 3.1};
+		double[] p1 = {-3,0.2};
+		double[] p2 = {7, -3};
 		Sample s1 = new Sample(p1);
 		Sample s2 = new Sample(p2);
 		System.out.println("s1="+s1.toString());
 		System.out.println("s2="+s2.toString());
+		System.out.println(s1.distanceTo(s2));
 		
 	}
 	

d69f3c95eeebf41c2fa68f513f5d59c34a958af3
denisezhong <62267168+denisezhong@users.noreply.github.com>
2020-04-18 22:11:13 -0400

    Added for loop in Optimization class

--- src/pa06/Optimization.java
+++ src/pa06/Optimization.java
@@ -3,20 +3,21 @@ package pa06;
 import java.io.FileNotFoundException;
 
 public class Optimization {
+	
+	private static int ITERATIONS = 100;
+	
     public static void main(String[] args) throws FileNotFoundException {
         KMeans kmeans = new KMeans();
         System.out.println(kmeans);
         System.out.println(kmeans.originalData);
         System.out.println();
-        kmeans.fillClusters();
-        System.out.println(kmeans);
-        kmeans.tick();
-        System.out.println(kmeans);
-        kmeans.fillClusters();
-        System.out.println(kmeans);
-        kmeans.tick();
-        System.out.println(kmeans);
-
+        
+        for(int i=0; i < ITERATIONS; i++) {
+        	kmeans.fillClusters();
+            System.out.println(kmeans);
+            kmeans.tick();
+            System.out.println(kmeans);
+        }
     }
 
 

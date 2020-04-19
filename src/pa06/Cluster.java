126b34eda6d698d9367bce995b12f052db5f3fb1
kyrarivest <57569515+kyrarivest@users.noreply.github.com>
2020-04-18 20:45:35 -0400

    added comments

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -31,15 +31,7 @@ public class Cluster {
         this.clusterpt = newpoint;
     }
 
-    public void cleanCluster() {
-        Iterator<Sample> itr = cluster.iterator();
-        while(itr.hasNext()){
-            itr.next();
-            itr.remove();
-        }
-    }
-
-    //randomly a sample from orginal data, then put it as clusterpt.
+    //randomly chooses a sample from orginal data, then put it as clusterpt.
     public Sample RdClusterpt(Cluster originaldata) {
         Random rd = new Random();
         int rdindex = rd.nextInt(originaldata.cluster.size());
@@ -64,7 +56,7 @@ public class Cluster {
     public String toString(){
         int i = 0;
         String output = "";
-        if(clusterpt.sample.size() > 0) {
+        if(clusterpt != null) {
             output += "Cluster pt. : " + clusterpt.toString() +  " Cluster samples : ";
         }else{
             output +=  "originalData samples : ";
